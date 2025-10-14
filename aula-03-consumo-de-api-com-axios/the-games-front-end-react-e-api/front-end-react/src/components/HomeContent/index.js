4b37ca0d716266a0d687a5e0916299300d5cb977
DSM3 <fatec-dsm3@academico.intra>
2025-10-14 20:26:35 -0300

    @catypr

---
+++ aula-03-consumo-de-api-com-axios/the-games-front-end-react-e-api/front-end-react/src/components/HomeContent/index.js
@@ -0,0 +1,46 @@
+// Importando useState
+import { useState, useEffect } from "react";
+// O hook useEffect é executado toda vez que o componente é re-renderizado (efeito colateral)
+
+import styles from "@/components/HomeContent/HomeContent.module.css";
+import Loading from "../Loading";
+// Importando o AXIOS (para enviar as requisições HTTP)
+import axios from "axios";
+
+const HomeContent = () => {
+  // Criando um estado para lista de jogos
+  const [games, setGames] = useState([]);
+
+  // Criando o bloco de useEffect:
+  useEffect(() => {
+    const fetchGames = async() => {
+      try {
+        const response = await axios.get("http://localhost:4000/games")
+        console.log(response)
+      }catch (error) {
+        console.loge(error);
+      }
+    };
+    fetchGames();
+  }, [games]); // dependência do useEffect 
+
+  return (
+    <>
+      <div className={styles.homeContent}>
+        {/* CARD LISTA DE JOGOS */}
+        <div className={styles.listGamesCard}>
+          {/* TITLE */}
+          <div className={styles.title}>
+            <h2>Lista de jogos</h2>
+          </div>
+          <Loading />
+          <div className={styles.games} id={styles.games}>
+            {/* Lista de jogos irá aqui */}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomeContent;
